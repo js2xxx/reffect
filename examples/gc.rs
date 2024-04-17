dac6026d74bf9c39edac51d62ca19d9b223bdb6a
徐启航 <akucxy@163.com>
2024-04-17 23:07:17 +0800

    Add stack-scanning GC example using effects

---
+++ examples/gc.rs
@@ -0,0 +1,245 @@
+#![feature(coroutines, coroutine_trait)]
+#![feature(impl_trait_in_assoc_type)]
+#![feature(lifetime_capture_rules_2024)]
+#![feature(strict_provenance)]
+
+use std::{
+    alloc::Layout,
+    collections::HashMap,
+    mem,
+    num::NonZeroUsize,
+    ops::{Deref, DerefMut},
+    ptr::NonNull,
+};
+
+use reffect::{
+    catch,
+    effect::{EffectList, ResumeTy},
+    effectful, effectful_block,
+    util::{
+        sum_type::{range::SplitList, repr::Split, Rem, RemTags},
+        tag::Tag,
+    },
+    Effect, Effectful, EffectfulExt,
+};
+
+#[repr(transparent)]
+struct Gc<T: ?Sized>(*mut T);
+
+impl<T: ?Sized> Drop for Gc<T> {
+    fn drop(&mut self) {
+        self.0 = self.0.with_addr(0);
+        println!("dropping reference of {self:p}");
+    }
+}
+
+impl<T> Deref for Gc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.0 }
+    }
+}
+
+impl<T> DerefMut for Gc<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.0 }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Alloc {
+    layout: Layout,
+    zero: bool,
+    drop: unsafe fn(NonNull<()>),
+}
+
+impl Effect for Alloc {
+    type Resume = NonNull<()>;
+}
+
+#[effectful(Alloc)]
+fn gc<T>(t: T) -> Gc<T> {
+    unsafe fn drop_ptr<T>(ptr: NonNull<()>) {
+        unsafe { core::ptr::drop_in_place(ptr.as_ptr().cast::<T>()) }
+    }
+
+    let ptr = yield Alloc {
+        layout: Layout::new::<T>(),
+        zero: false,
+        drop: drop_ptr::<T>,
+    };
+    let ptr = ptr.cast::<T>();
+    unsafe { ptr.as_ptr().write(t) };
+
+    Gc(ptr.as_ptr())
+}
+
+/// # Safety
+///
+/// - `allocate` must allocate memory according the request in `Alloc` and
+///   return a valid pointer to the allocated memory.
+unsafe trait Collector {
+    unsafe fn allocate(&mut self, alloc: Alloc) -> Option<NonNull<()>>;
+
+    unsafe fn mark(&mut self, address: NonZeroUsize);
+
+    unsafe fn reclaim(&mut self);
+}
+
+trait CollectExt<E, U>: Effectful<E> + Sized
+where
+    E: EffectList + Split<Alloc, U>,
+
+    E::ResumeList: Split<ResumeTy<Alloc>, U, Remainder = <Rem<E, Alloc, U> as EffectList>::ResumeList>
+        + SplitList<
+            Rem<E::ResumeList, ResumeTy<Alloc>, U>,
+            RemTags<E::ResumeList, ResumeTy<Alloc>, U>,
+        >,
+    U: Tag,
+
+    Rem<E, Alloc, U>: EffectList,
+{
+    #[effectful(static; ...Rem<E, Alloc, U>)]
+    fn collect<C: Collector>(self, collector: &mut C) -> Self::Return {
+        let this = self;
+        let mut reclaimed = false;
+        let coro = core::pin::pin!(this);
+
+        let ptr = (&*coro.as_ref() as *const Self).cast::<usize>();
+        assert!(ptr.is_aligned());
+
+        catch!(coro.await {
+            #![non_exhaustive]
+
+            alloc @ Alloc { .. } => loop {
+                if let Some(ptr) = unsafe { collector.allocate(alloc) } {
+                    reclaimed = false;
+                    break ptr;
+                }
+
+                if !reclaimed {
+                    unsafe {
+                        (0..(mem::size_of::<Self>() / mem::size_of::<usize>()))
+                            .filter_map(|delta| {
+                                let value = ptr.add(delta).read();
+                                println!("{:p} = {:#x}", ptr.add(delta), value);
+                                NonZeroUsize::new(value)
+                            })
+                            .for_each(|addr| collector.mark(addr));
+
+                        collector.reclaim();
+                    }
+                    reclaimed = true;
+                } else {
+                    panic!("memory exhausted")
+                }
+            }
+        })
+    }
+}
+
+impl<Coro, E, U> CollectExt<E, U> for Coro
+where
+    Coro: Effectful<E>,
+    E: EffectList + Split<Alloc, U>,
+
+    E::ResumeList: Split<ResumeTy<Alloc>, U, Remainder = <Rem<E, Alloc, U> as EffectList>::ResumeList>
+        + SplitList<
+            Rem<E::ResumeList, ResumeTy<Alloc>, U>,
+            RemTags<E::ResumeList, ResumeTy<Alloc>, U>,
+        >,
+    U: Tag,
+
+    Rem<E, Alloc, U>: EffectList,
+{
+}
+
+struct AllocData {
+    ptr: NonNull<()>,
+    layout: Layout,
+    ref_count: usize,
+    drop: unsafe fn(NonNull<()>),
+}
+
+struct SimpleCollector {
+    map: HashMap<NonZeroUsize, AllocData>,
+    heap_size: usize,
+    heap_limit: usize,
+}
+
+impl SimpleCollector {
+    fn new(heap_limit: usize) -> Self {
+        Self {
+            map: HashMap::new(),
+            heap_size: 0,
+            heap_limit,
+        }
+    }
+}
+
+unsafe impl Collector for SimpleCollector {
+    unsafe fn allocate(&mut self, alloc: Alloc) -> Option<NonNull<()>> {
+        if self.heap_size + alloc.layout.size() > self.heap_limit {
+            println!("heap is full");
+            return None;
+        }
+
+        let ptr = if alloc.zero {
+            std::alloc::alloc_zeroed(alloc.layout)
+        } else {
+            std::alloc::alloc(alloc.layout)
+        };
+        let ptr = NonNull::new(ptr.cast())?;
+        println!("allocated {:?} of size {:?}", ptr, alloc.layout.size());
+
+        self.heap_size += alloc.layout.size();
+        self.map.insert(ptr.addr(), AllocData {
+            ptr,
+            layout: alloc.layout,
+            ref_count: 0,
+            drop: alloc.drop,
+        });
+        Some(ptr)
+    }
+
+    unsafe fn mark(&mut self, address: NonZeroUsize) {
+        if let Some(data) = self.map.get_mut(&address) {
+            println!("marking {:#x}", address);
+            data.ref_count += 1;
+        }
+    }
+
+    unsafe fn reclaim(&mut self) {
+        self.map.retain(|_, data| {
+            let referred = mem::replace(&mut data.ref_count, 0) > 0;
+            if !referred {
+                println!("dropping {:p}", data.ptr);
+                unsafe { (data.drop)(data.ptr) };
+                self.heap_size -= data.layout.size();
+                unsafe { std::alloc::dealloc(data.ptr.as_ptr().cast(), data.layout) };
+            }
+            referred
+        })
+    }
+}
+
+fn main() {
+    let f = effectful_block! {
+        #![effectful(Alloc)]
+
+        let obj2 = {
+            let obj1 = gc(1000).await;
+            println!("obj1 is at {:p}", &obj1);
+            let obj2 = gc(67890).await;
+            println!("obj2 is at {:p}", &obj2);
+            obj2
+        };
+        let obj3 = gc(1).await;
+        println!("obj3 is at {:p}", &obj3);
+        *obj2 + *obj3
+    };
+
+    let mut c = SimpleCollector::new(8);
+    assert_eq!(f.collect(&mut c).run(), 67891);
+}
